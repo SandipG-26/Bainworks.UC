77ba1afcc892a95b44272690c8b6343c2fb1b142
SandipG-26 <70084836+SandipG-26@users.noreply.github.com>
2020-10-02 09:11:20 +0530

    all

---
+++ Brainworks.UC.Data/UCcontext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Brainworks.UC.Data
+{
+    public class UCcontext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionstring = "Server=DESKTOP-6N1BUO9;Database=SQLdatabase;UserId=SG_user;Password=sandip21";
+            optionsBuilder.UseSqlServer(connectionstring);
+        }
+    }
+}

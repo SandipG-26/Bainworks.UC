0dcfdeed78341739c78636b635c2194133335c17
SandipG-26 <70084836+SandipG-26@users.noreply.github.com>
2020-12-13 16:59:36 +0530

    second axios try

--- Bainworks.UC.Persistance/Migrations/20201213112713_axios added.Designer.cs
+++ Bainworks.UC.Persistance/Migrations/20201213112713_axios added.Designer.cs
@@ -4,14 +4,16 @@ using Brainworks.UC.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Brainworks.UC.Persistance.Migrations
 {
     [DbContext(typeof(UCcontext))]
-    partial class UCcontextModelSnapshot : ModelSnapshot
+    [Migration("20201213112713_axios added")]
+    partial class axiosadded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Bainworks.UC.Persistance/Migrations/20201213112713_axios added.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Brainworks.UC.Persistance.Migrations
+{
+    public partial class axiosadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "feedback",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "feedback",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
